8c741f6ec38612ceb19a0afbeafa2994efd072c0
TrevorFrench <trevorf96@gmail.com>
2019-11-30 20:41:43 -0800

    ffd

--- server.js
+++ server.js
@@ -184,7 +184,7 @@ https.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED
   // The whole response has been received. Print out the result.
   resp.on('end', () => {
     
-    res.render('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=' + req.body.ticker + '&apikey=2XZVVF334ODD3HNT&datatype=csv', {statusMessage: "Downloaded Successfully."});
+    res.redirect('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=' + req.body.ticker + '&apikey=2XZVVF334ODD3HNT&datatype=csv');
     
   });
 
