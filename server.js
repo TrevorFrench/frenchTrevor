f97a470cbbc80ee8df890e79671728daca708b54
TrevorFrench <trevorf96@gmail.com>
2019-11-30 20:45:19 -0800

    ffd

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ https.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=M
   // The whole response has been received. Print out the result.
   resp.on('end', () => {
 
-    let textTwo = "<form name='form1' method='POST' action='/frenchsFinancialDataResp'><input type='text' name='ticker'><input type='submit'></form><form name='form2' method='POST' action='/frenchsFinancialDataCSV'><input type='text' name='ticker'><input type='submit'></form>";
+    let textTwo = "<form name='form1' method='POST' action='/frenchsFinancialDataResp'><input type='text' name='ticker'><input type='submit'></form><form name='form2' method='GET' action='/frenchsFinancialDataCSV'><input type='text' name='ticker'><input type='submit'></form>";
     
     res.render('frenchsFinancialData.ejs', {statusMessage: textTwo});
     
@@ -168,7 +168,7 @@ https.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='
 //****FRENCH'S Financial Data CSV***********
 //******************************************
 //******************************************
-app.post('/frenchsFinancialDataCSV', function(req, res) {
+app.get('/frenchsFinancialDataCSV', function(req, res) {
   const https = require('https');
 
   console.log(req.body.ticker);
