0170c89a34654f5d8dd90fca0774fdccbd600ec1
TrevorFrench <trevorf96@gmail.com>
2019-11-30 16:45:47 -0800

    creating french's financial data

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ https.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=M
   // The whole response has been received. Print out the result.
   resp.on('end', () => {
 
-    let text = JSON.parse(data)['Meta Data'];
+    let text = JSON.parse(data)['Meta Data'][0];
 
     
     res.render('frenchsFinancialData.ejs', {statusMessage: text});
